b1f39f60f86a14381f4ad9cb2b797efd0b7ff559
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-07-11 15:51:49 +0800

    feat: add login_failure.hbs

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const pseudoData = require('../models/pseudoData')
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.post('/', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const welcomeMessage = ''
+
+  const loginCheck = pseudoData.find(element => {
+    return element.email === email && element.password === password
+  })
+
+  if (loginCheck === undefined) {
+    res.render('login_failure')
+  } else {
+    const welcomeMessage = loginCheck.firstName
+    res.render('welcome', { welcomeMessage })
+  }
+
 })
 
 module.exports = router
